b6480f60ff1b3d623a3945cca3311b7588b907e9
ariannajguiao <ariannajgguiao@gmail.com>
2020-07-27 23:57:33 +0800

    header, navbar brand update

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,33 +1,49 @@
-var swiperBanner = new Swiper('.swiper-container.swiper-banner', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-});
+$(document).ready(function () {
+  var swiperBanner = new Swiper(".swiper-container.swiper-banner", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+  });
 
-var swiperTestimonials = new Swiper('.swiper-container.swiper-testimonial', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  loop: true,
-  autoplay: {
-    delay: 3500,
-    disableOnInteraction: false,
-  }
-});
+  var swiperTestimonials = new Swiper(".swiper-container.swiper-testimonial", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+    // autoplay: {
+    //   delay: 3500,
+    //   disableOnInteraction: false,
+    // },
+  });
 
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if($(this).scrollTop() < $('.section-banner').height()) { 
-        $('.navbar').removeClass('solid');
+  $("#navbarSupportedContent").on("show.bs.collapse", function () {
+    $(".navbar").addClass("bg-white-toggle");
+  });
+
+  $("#navbarSupportedContent").on("hide.bs.collapse", function () {
+    $(".navbar").removeClass("bg-white-toggle");
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() < $(".section-banner").height()) {
+      $(".navbar").removeClass("bg-white");
+      $(".navbar .navbar-brand img").attr(
+        "src",
+        "./assets/img/prohandy logo-white.svg"
+      );
     } else {
-        $('.navbar').addClass('solid');
+      $(".navbar").addClass("bg-white");
+      $(".navbar .navbar-brand img").attr(
+        "src",
+        "./assets/img/prohandy logo.svg"
+      );
     }
   });
 });
